579929c1a7b2ea9c4c1434e88bc6450f0e003e6a
Excel <000.excel@gmail.com>
2020-09-20 10:35:22 +0530

    Promise tut

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
-console.log("He0");
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+// const readDBFile = new Promise((resolve, reject) => {
+//   fs.readFile("./db.json", (err, data) => {
+//     if (err) {
+//       resolve(err);
+//     } else {
+//       resolve(JSON.parse(data.toString()));
+//     }
+//   });
+// });
+
+const cb = (err, data, resolve) => {
+  if (err) {
+    resolve(err);
+  } else {
+    resolve(JSON.parse(data.toString()));
+  }
+};
+
+const readDBFile = new Promise((resolve, reject) => {
+  fs.readFile("./db.json", (err, data) => {
+    cb(err, data, resolve, "Hello");
+  });
+});
+
+app.get("/profiles", (req, res) => {
+  readDBFile.then((d) => {
+    res.send(d.users);
+  });
+});
+
+app.get("/posts", (req, res) => {
+  readDBFile.then((d) => {
+    res.send(d.posts);
+  });
+});
+app.get("/comments", (req, res) => {
+  readDBFile.then((d) => {
+    res.send(d.comments);
+  });
+});
+
+app.post("/profile", (req, res) => {
+  readDBFile.then((d) => {
+    res.send(d.users);
+  });
+});
+app.post("/posts", (req, res) => {
+  readDBFile.then((d) => {
+    res.send(d.posts);
+  });
+});
+app.post("/comments", (req, res) => {
+  readDBFile.then((d) => {
+    res.send(d.comments);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
