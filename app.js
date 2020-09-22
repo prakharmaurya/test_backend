50fa16d5a656ae7c933786134d1ac22621e40733
Excel <000.excel@gmail.com>
2020-09-22 10:15:47 +0530

    db write fn implemented

--- app.js
+++ app.js
@@ -3,27 +3,47 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 
+const readDBFile = new Promise((resolve, reject) => {
+  fs.readFile("./db.json", (err, data) => {
+    if (err) {
+      resolve(err);
+    } else {
+      resolve(JSON.parse(data.toString()));
+    }
+  });
+});
+
+const writeDBFile = (d) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./db.json", JSON.stringify(d), (err) => {
+      if (err) {
+        resolve(err);
+      } else {
+        resolve(d.users[d.users.length - 1]);
+      }
+    });
+  });
+};
+
+// const cb = (err, data, resolve, reject) => {
+//   if (err) {
+//     resolve(err);
+//   } else {
+//     resolve(JSON.parse(data.toString()));
+//   }
+// };
+
 // const readDBFile = new Promise((resolve, reject) => {
 //   fs.readFile("./db.json", (err, data) => {
-//     if (err) {
-//       resolve(err);
-//     } else {
-//       resolve(JSON.parse(data.toString()));
-//     }
+//     cb(err, data, resolve, reject);
 //   });
 // });
 
-const cb = (err, data, resolve) => {
-  if (err) {
-    resolve(err);
-  } else {
-    resolve(JSON.parse(data.toString()));
-  }
-};
+app.use(express.json());
 
-const readDBFile = new Promise((resolve, reject) => {
-  fs.readFile("./db.json", (err, data) => {
-    cb(err, data, resolve, "Hello");
+app.get("/posts", (req, res) => {
+  readDBFile.then((d) => {
+    res.send(d.posts);
   });
 });
 
@@ -33,11 +53,6 @@ app.get("/profiles", (req, res) => {
   });
 });
 
-app.get("/posts", (req, res) => {
-  readDBFile.then((d) => {
-    res.send(d.posts);
-  });
-});
 app.get("/comments", (req, res) => {
   readDBFile.then((d) => {
     res.send(d.comments);
@@ -45,19 +60,35 @@ app.get("/comments", (req, res) => {
 });
 
 app.post("/profile", (req, res) => {
+  console.log(req.body);
   readDBFile.then((d) => {
-    res.send(d.users);
+    // edit d
+    d.users.push({
+      id: d.users[d.users.length - 1].id + 1,
+      name: req.body.name,
+      age: req.body.age,
+    });
+
+    writeDBFile(d).then((dx) => {
+      res.status(201).send(dx);
+    });
   });
 });
-app.post("/posts", (req, res) => {
+
+app.post("/post", (req, res) => {
   readDBFile.then((d) => {
     res.send(d.posts);
   });
 });
-app.post("/comments", (req, res) => {
+
+app.post("/comment", (req, res) => {
   readDBFile.then((d) => {
     res.send(d.comments);
   });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("page not found");
 });
 
 app.listen(port, () => {
